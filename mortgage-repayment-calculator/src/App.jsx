e9f09653b398594df697b268afe747d65b1c1340
Pietrell <piotrplotast2006@gmail.com>
2024-09-15 11:12:01 +0200

    add tailwind to the project

--- mortgage-repayment-calculator/src/App.jsx
+++ mortgage-repayment-calculator/src/App.jsx
@@ -1,8 +1,9 @@
+import "./index.css";
 function App() {
   return (
     <>
       <div>
-        <h1>add files</h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </div>
     </>
   );
